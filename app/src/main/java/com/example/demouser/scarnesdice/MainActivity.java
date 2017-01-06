39136d9cec4b4ca6c2f9d475a70fabadebf7b65e
Kat Aiello <aiell22k@mtholyoke.edu>
2017-01-06 14:52:57 -0500

    Finished v2 and started v3

--- app/src/main/java/com/example/demouser/scarnesdice/MainActivity.java
+++ app/src/main/java/com/example/demouser/scarnesdice/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.demouser.scarnesdice;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    public int currentTurn;
-    public int playerTotal;
-    public int computerTotal;
+    public int currentTurn = 0;
+    public int playerTotal = 0;
+    public int computerTotal = 0;
+    public int round = 0;
+    public TextView computerScoreText;
+    public TextView playerScoreText;
+    public TextView turnScoreText;
+    public TextView rollText;
+    public ImageView dieView;
+    public final Handler timerHandler = new Handler();
     public Players whosTurn;
     public Random random;
     public enum Players {
@@ -26,8 +35,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        resetNoView();
+
         random = new Random();
         whosTurn = Players.PLAYER;
+
+        dieView = (ImageView) findViewById(R.id.dieView);
+        playerScoreText = (TextView) findViewById(R.id.player1Score);
+        computerScoreText = (TextView) findViewById(R.id.player2Score);
+        turnScoreText = (TextView) findViewById(R.id.turnScoreText);
+        rollText = (TextView) findViewById(R.id.rollText);
+
 
 
         ((Button)findViewById(R.id.rollButton)).setOnClickListener(new View.OnClickListener()
@@ -60,13 +78,31 @@ public class MainActivity extends AppCompatActivity {
 
     public void roll(View view)
     {
+        rollNoView();
+    }
+
+    public void rollNoView()
+    {
         //generate a random number between 1 and 6 inclusive
         int roll = rollDice();
         //show it in rollText
-        ((TextView) findViewById(R.id.rollText)).setText(Integer.toString(roll));
-        //TODO implement with images instead of text
-        
-
+        //rollText.setText(Integer.toString(roll));
+        //implemented with images instead of text
+        switch(roll)
+        {
+            case 1: dieView.setImageResource(R.drawable.dice1);
+                break;
+            case 2: dieView.setImageResource(R.drawable.dice2);
+                break;
+            case 3: dieView.setImageResource(R.drawable.dice3);
+                break;
+            case 4: dieView.setImageResource(R.drawable.dice4);
+                break;
+            case 5: dieView.setImageResource(R.drawable.dice5);
+                break;
+            case 6: dieView.setImageResource(R.drawable.dice6);
+                break;
+        }
 
 
         //if the roll is 1, automatically change players, don't add the score
@@ -74,19 +110,23 @@ public class MainActivity extends AppCompatActivity {
         {
             changePlayers();
             String lostRound = String.format("You rolled a 1! Your score was \n reset and the players switched. \n It is now %s's turn.", whosTurn.toString());
-            ((TextView) findViewById(R.id.turnScoreText)).setText(lostRound);
+            turnScoreText.setText(lostRound);
         }
         //else, add it to the score
         else
         {
             currentTurn += roll;
             //show it in turnScoreText
-            ((TextView) findViewById(R.id.turnScoreText)).setText(Integer.toString(currentTurn));
+            turnScoreText.setText(Integer.toString(currentTurn));
         }
-
     }
 
     public void hold(View view)
+    {
+        holdNoView();
+    }
+
+    public void holdNoView()
     {
         //add to appropriate player's score
         if(whosTurn.equals(Players.PLAYER))
@@ -105,10 +145,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void reset(View view)
     {
+        resetNoView();
+    }
+
+    public void resetNoView()
+    {
         resetScores();
-        ((TextView) findViewById(R.id.player1Score)).setText("0");
-        ((TextView) findViewById(R.id.player2Score)).setText("0");
-        ((TextView) findViewById(R.id.turnScoreText)).setText("0");
+        playerScoreText.setText("0");
+        computerScoreText.setText("0");
+        turnScoreText.setText("0");
     }
 
     private int rollDice()
@@ -129,8 +174,13 @@ public class MainActivity extends AppCompatActivity {
         else
         {
             whosTurn = Players.COMPUTER;
-            //computerTurn();
+            round = 0;
+            computerTurnIn500();
         }
+
+        String turnText = String.format("Players switched, it is now %s's turn.", whosTurn.toString());
+        turnScoreText.setText(turnText);
+
     }
 
     /**
@@ -142,7 +192,7 @@ public class MainActivity extends AppCompatActivity {
         //add the turn score to the total
         playerTotal += turnTotal;
         //update player1Score
-        ((TextView) findViewById(R.id.player1Score)).setText(Integer.toString(playerTotal));
+        playerScoreText.setText(Integer.toString(playerTotal));
     }
 
     /**
@@ -154,7 +204,7 @@ public class MainActivity extends AppCompatActivity {
         //add the turn score to the total
         computerTotal += turnTotal;
         //update player2Score
-        ((TextView) findViewById(R.id.player2Score)).setText(Integer.toString(computerTotal));
+        computerScoreText.setText(Integer.toString(computerTotal));
     }
 
     /**
@@ -165,18 +215,63 @@ public class MainActivity extends AppCompatActivity {
         currentTurn = 0;
         playerTotal = 0;
         computerTotal = 0;
+        round = 0;
     }
 
-    //TODO implement
+
     private void computerTurn()
     {
-        //TODO roll first
-        //TODO timer -- wait
-        while(whosTurn.equals(Players.COMPUTER))
+        rollNoView();
+        if(shouldHold(round))
         {
-            //TODO each time randomly choose to roll or hold
-            //TODO timer -- wait
+            holdNoView();
         }
+        else
+        {
+            rollNoView();
+        }
+    }
+
+    private boolean shouldHold(int roundNum)
+    {
+        //the expected sum for this many turns
+        double expect = roundNum * 3.5;
+
+        //if we have averaged one more than the expectation each round, hold
+        if(currentTurn > expect + roundNum)
+        {
+            return true;
+        }
+        else
+        {
+            //hold if we have had more than 6 rounds
+            if(roundNum > 6)
+            {
+                return true;
+            }
+            else
+            {
+                //otherwise, hold only if we have more than 15 points (sizeable)
+                return (currentTurn > 15);
+            }
+        }
+    }
+
+    private void computerTurnIn500()
+    {
+        timerHandler.postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                round++;
+                computerTurn();
+                if(whosTurn == Players.COMPUTER)
+                {
+                    computerTurnIn500();
+                }
+            }
+        }, 500);
     }
 
 }
